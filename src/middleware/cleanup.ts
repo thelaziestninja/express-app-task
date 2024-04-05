a123d2c0b465eacc6b0cc9bbf4409ddffa0edfcd
Godji Berry <verginia.razvigorova@gmail.com>
2024-04-05 09:24:50 +0300

    Working on cleanup method

--- src/middleware/cleanup.ts
+++ src/middleware/cleanup.ts
@@ -3,106 +3,86 @@ import logger from "../utils/logger";
 import { Request, Response, NextFunction } from "express";
 import { maxKeys, threshold } from "../../config/default";
 import { storeWithTTL, storeWithoutTTL } from "../controller/store.controller";
+// const sortKeysWithoutTTLForCleanup = async () => {
+//   try {
+//     let sortedKeysWithoutTTL: string[] = [];
+//     const totalKeysWithoutTTL = storeWithoutTTL.size;
+//     const totalKeysWithTTL = storeWithTTL.size;
 
-const sortKeysWithoutTTLForCleanup = async () => {
+//     logger.info(
+//       `Total keys in store: ${
+//         totalKeysWithoutTTL + totalKeysWithTTL
+//       }, max keys allowed: ${maxKeys * threshold}`
+//     );
+
+//     if (totalKeysWithoutTTL + totalKeysWithTTL > maxKeys * threshold) {
+//       logger.info(
+//         `Cleanup keys without TTL triggered as total keys are greater than or equal to ${
+//           maxKeys * threshold
+//         }`
+//       );
+
+//       sortedKeysWithoutTTL = Array.from(storeWithoutTTL.keys()).sort((a, b) => {
+//         const aItem = storeWithoutTTL.get(a);
+//         const bItem = storeWithoutTTL.get(b);
+//         if ((aItem?.count ?? 0) !== (bItem?.count ?? 0)) {
+//           return (aItem?.count ?? 0) - (bItem?.count ?? 0);
+//         } else {
+//           return (
+//             (aItem?.created_at?.getTime() ?? 0) -
+//             (bItem?.created_at?.getTime() ?? 0)
+//           );
+//         }
+//       });
+//     }
+
+//     return sortedKeysWithoutTTL;
+//   } catch (e: any) {
+//     logger.info(`Error in sortKeysWithoutTTLForCleanup method: ${e.message}`);
+//     return [];
+//   }
+// };
+
+const sortAndDeleteKeysWithoutTTL = async () => {
   try {
-    let sortedKeysWithoutTTL: string[] = [];
-    const totalKeysWithoutTTL = storeWithoutTTL.size;
-    const totalKeysWithTTL = storeWithTTL.size;
+    const totalKeys = storeWithoutTTL.size + storeWithTTL.size;
+    const deletionThreshold = maxKeys * threshold;
 
-    logger.info(
-      `Total keys in store: ${
-        totalKeysWithoutTTL + totalKeysWithTTL
-      }, max keys allowed: ${maxKeys * threshold}`
-    );
-
-    if (totalKeysWithoutTTL + totalKeysWithTTL > maxKeys * threshold) {
+    if (totalKeys > deletionThreshold) {
       logger.info(
-        `Cleanup keys without TTL triggered as total keys are greater than or equal to ${
-          maxKeys * threshold
-        }`
+        "Sorting keys without TTL for cleanup as the total keys exceed threshold"
       );
 
-      sortedKeysWithoutTTL = Array.from(storeWithoutTTL.keys()).sort((a, b) => {
-        const aItem = storeWithoutTTL.get(a);
-        const bItem = storeWithoutTTL.get(b);
-        if ((aItem?.count ?? 0) !== (bItem?.count ?? 0)) {
-          return (aItem?.count ?? 0) - (bItem?.count ?? 0);
+      const entries = Array.from(storeWithoutTTL.entries());
+
+      const sortedKeysWithoutTTL = entries.sort(([, a], [, b]) => {
+        const aCreatedAt = a.created_at.getTime();
+        const bCreatedAt = b.created_at.getTime();
+
+        if (a.count !== undefined && b.count !== undefined) {
+          return a.count - b.count;
         } else {
-          return (
-            (aItem?.created_at?.getTime() ?? 0) -
-            (bItem?.created_at?.getTime() ?? 0)
-          );
+          return aCreatedAt - bCreatedAt;
         }
       });
+
+      for (const [key, value] of sortedKeysWithoutTTL) {
+        if (storeWithoutTTL.has(key) && !value.ttl) {
+          storeWithoutTTL.delete(key);
+          logger.info(`Deleted key(${key}) without TTL`);
+        }
+
+        if (storeWithoutTTL.size + storeWithTTL.size <= deletionThreshold) {
+          break;
+        }
+      }
     }
 
-    return sortedKeysWithoutTTL;
+    logger.info("Total keys are within the threshold. No cleanup needed.");
+    return;
   } catch (e: any) {
     logger.info(`Error in sortKeysWithoutTTLForCleanup method: ${e.message}`);
-    return [];
-  }
-};
-
-const sortKeysWithTTLForCleanup = async (): Promise<string[]> => {
-  try {
-    let sortedKeysWithTTL: string[] = [];
-    const totalKeysWithoutTTL = storeWithoutTTL.size;
-    const totalKeysWithTTL = storeWithTTL.size;
-
-    logger.info(
-      `Total keys in store: ${
-        totalKeysWithoutTTL + totalKeysWithTTL
-      }, max keys allowed: ${maxKeys * threshold}`
-    );
-
-    if (totalKeysWithoutTTL + totalKeysWithTTL > maxKeys * threshold) {
-      logger.info(
-        `Cleanup keys with TTL triggered as total keys are greater than or equal to ${
-          maxKeys * threshold
-        }`
-      );
-
-      const keysWithTTL = Array.from(storeWithTTL.keys());
-      const items = await Promise.all(
-        keysWithTTL.map(async (key) => {
-          const item = storeWithTTL.get(key);
-          return {
-            key,
-            count: item?.count ?? 0,
-            created_at: item?.created_at?.getTime() ?? 0,
-          };
-        })
-      );
-
-      sortedKeysWithTTL = keysWithTTL.sort((a, b) => {
-        const aItem = items.find((item) => item.key === a);
-        const bItem = items.find((item) => item.key === b);
-        if (aItem && bItem) {
-          if (aItem.count !== bItem.count) {
-            return aItem.count - bItem.count;
-          } else {
-            return aItem.created_at - bItem.created_at;
-          }
-        }
-        return 0;
-      });
-    }
-
-    return sortedKeysWithTTL;
-  } catch (e: any) {
-    logger.info(`Error in sortKeysWithTTLForCleanup method: ${e.message}`);
-    return [];
-  }
-};
-
-const deleteKeysWithoutTTL = async (keysWithoutTTLToDelete: string[]) => {
-  try {
-    for (const keyWithoutTTLToDelete of keysWithoutTTLToDelete) {
-      storeWithoutTTL.delete(keyWithoutTTLToDelete);
-    }
-  } catch (e: any) {
-    logger.info(`Error in deleteKeysWithoutTTL method: ${e.message}`);
+    throw e;
   }
 };
 
@@ -113,53 +93,9 @@ export const cleanupKeys = async (
 ) => {
   try {
     logger.info("Started checking for keys without TTL to delete in store");
-    const sortedKeysWithoutTTL = await sortKeysWithoutTTLForCleanup();
-    const sortedKeysWithTTL = await sortKeysWithTTLForCleanup();
 
-    if (sortedKeysWithoutTTL.length) {
-      const keysToDelete: string[] = [];
-
-      for (const key of sortedKeysWithoutTTL) {
-        const item = storeWithoutTTL.get(key);
-        if (!item || item.ttl) {
-          logger.info(`Skipping key(${key}) deletion as it has ttl set`);
-          continue;
-        }
-
-        keysToDelete.push(key);
-        logger.info(`Key(${key}) added to delete list`);
-        if (
-          keysToDelete.length >
-          storeWithoutTTL.size + storeWithTTL.size - maxKeys
-        ) {
-          break;
-        }
-      }
-      logger.info(`Deleting Keys without TTL: ${keysToDelete.join(",")}`);
-      await deleteKeysWithoutTTL(keysToDelete);
-    }
-
+    await sortAndDeleteKeysWithoutTTL();
     // if keys still exceed the threshold, delete the least used and oldest keys with ttl
-    const totalKeys = storeWithoutTTL.size + storeWithTTL.size;
-    if (totalKeys > maxKeys * threshold) {
-      logger.info(
-        "Started cleaning keys with TTL as total keys still exceed threshold"
-      );
-      const keysToDelete: string[] = [];
-
-      // Use sortedKeysWithTTL here
-      for (const key of sortedKeysWithTTL) {
-        keysToDelete.push(key);
-        if (keysToDelete.length >= totalKeys - maxKeys * threshold) {
-          break;
-        }
-      }
-
-      logger.info(`Deleting Keys with TTL: ${keysToDelete.join(",")}`);
-      keysToDelete.forEach((key) => {
-        storeWithTTL.delete(key);
-      });
-    }
 
     next();
   } catch (e: any) {
